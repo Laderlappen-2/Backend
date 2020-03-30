11fbb887c05d3a731c22ccce3cb398b4792f0597
Adam <a.clettborn@gmail.com>
2020-03-30 13:01:52 +0200

    Created project structure

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,42 @@
-const express = require("express")
+import * as dotenv from "dotenv"
+import express = require("express")
+import { loadControllers, scopePerRequest } from 'awilix-express'
+//import * as managers from "./business-logic-layer"
+import { sequelize } from "./data-layer/database"
+const { asClass, asValue, createContainer} = require('awilix')
 const app = express()
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log("REST API up and running on port " + (process.env.PORT || 8080))
+// Setup dotenv
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+// Setup awilix container
+const container = createContainer()
+container.register({
+    // Scoped lifetime = new instance per request
+    // Imagine the TodosService needs a `user`.
+    // class TodosService { constructor({ user }) { } }
+    //userManager: asClass(managers.UserManager)
 })
+
+// Sync database
+sequelize.sync()
+    .then(() => {
+        console.log("Database synchronized, starting server..")
+        startServer(PORT)
+    })
+    .catch(err => {
+        console.log(err, "Error synchronizing database")
+    })
+
+// Setup awilix express
+app.use(scopePerRequest(container))
+app.use("/v1", loadControllers("presentation-layer/v1/*.js", { cwd: __dirname } ))
+
+function startServer(port: any) {
+    app.listen(port, () => {
+        console.log("REST API up and running on port " + port)
+    })
+}
+
---
+++ api/src/business-logic-layer/users.manager.ts
@@ -0,0 +1,9 @@
+/*
+import { User } from "../data-layer/models/user.model"
+
+export class UserManager {
+    constructor() {
+
+    }
+}
+*/
