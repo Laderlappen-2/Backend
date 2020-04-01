f1cc9d60cc6629fc95703fa03282dfe5469a7d77
Adam <a.clettborn@gmail.com>
2020-04-01 10:29:15 +0200

    Changed model structure

--- api/src/data-layer/database.ts
+++ api/src/data-layer/database.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize-typescript"
+import * as models from "./models"
 
 export const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'backend-db.sqlite',
-    models: [ __dirname + '/models/*.model.js' ],
+    models: Object.keys(models),
 })
--- api/src/data-layer/models/index.ts
+++ api/src/data-layer/models/index.ts
@@ -1,3 +1,5 @@
 export * from "./eventType.model"
 export * from "./session.model"
 export * from "./event.model"
+export * from "./collisionAvoidanceEvent.model"
+export * from "./positionEvent.model"
