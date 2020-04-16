2e7db07692ea1fd82c7c9626cd4db780601f22a5
Adam <a.clettborn@gmail.com>
2020-04-16 10:08:40 +0200

    LAD-83 #time 5m #comment Implemented endpoint to get all events, with pagination

--- api/src/business-logic-layer/events.manager.ts
+++ api/src/business-logic-layer/events.manager.ts
@@ -1,6 +1,48 @@
 import { PositionEvent, Event, EventTypeEnum, PositionEventType, EventType } from "../data-layer/models" 
+import { FindOptions, Op } from "sequelize"
+import { PaginationQuery, PaginationResult } from "./drivingSessions.manager"
 
 export class EventsManager {
+
+    // TODO Extend generic base class, as this exact method is used on multiple places
+    async getWithPagination(pagination?: PaginationQuery): Promise<PaginationResult<Event>> {
+        let options: FindOptions = {}
+
+        if(pagination) {
+            // Get all IDs above the id specified in "pagination.from" (Op.gt == Greater than)
+            if(pagination.from) {
+                options.where = {
+                    id: {
+                        [Op.gte]: pagination.from
+                    }
+                }
+            }
+            options.limit = pagination.limit ?? undefined
+        }
+
+
+        const result = await Event.findAll(options)
+        let nextResult: any = null
+
+        if(result.length > 0) {
+            // Try to get next id
+            nextResult = await Event.findOne({
+                where: {
+                    id: {
+                        // Get the id of the last row returned
+                        [Op.gt]: result[result.length-1].id
+                    }
+                }
+            })
+        }
+
+        return {
+            from: pagination && pagination.from ? pagination.from : null,
+            next: nextResult && nextResult.id ? nextResult.id : null,
+            limit: pagination && pagination.limit ? pagination.limit : 0,
+            results: result
+        }
+    }
 
     async create(options: CreateEventOptions): Promise<Event> {
         if(Object.keys(EventTypeEnum).indexOf(options.eventType.toString()) == -1) {
--- api/src/presentation-layer/v1/events.route.ts
+++ api/src/presentation-layer/v1/events.route.ts
@@ -1,6 +1,6 @@
 import { route, GET, POST, before } from 'awilix-express'
 import { Request, Response, NextFunction } from "express"
-import { EventsManager } from '../../business-logic-layer'
+import { EventsManager, PaginationQuery } from '../../business-logic-layer'
 
 @route('/events')
 export default class DrivingSessionsRoute {
@@ -9,6 +9,22 @@ export default class DrivingSessionsRoute {
     
     constructor({ eventsManager }) {
         this.eventsManager = eventsManager
+    }
+
+    @GET()
+    async getEvents(req: Request, res: Response, next: NextFunction) {
+        try {
+            const pagination: PaginationQuery = {}
+            
+            if(req.query.from)
+                pagination.from = parseInt(req.query.from)
+            if(req.query.limit)
+                pagination.limit = parseInt(req.query.limit)
+
+            res.json(await this.eventsManager.getWithPagination(pagination))
+        } catch(err) {
+            next(err)
+        }
     }
 
     @POST()
