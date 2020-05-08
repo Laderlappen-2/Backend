79196e69b63e5b20f1d7d859f2cce059f5c02840
Adam <a.clettborn@gmail.com>
2020-05-08 15:39:57 +0200

    Fixed docker and travis

--- api/src/data-layer/database.ts
+++ api/src/data-layer/database.ts
@@ -1,21 +1,7 @@
 import { Sequelize } from "sequelize-typescript"
 
-// export const sequelize = new Sequelize({
-//     dialect: 'sqlite',
-//     storage: 'backend-db.sqlite',
-//     models: [__dirname + "/models/*.model.ts"],
-//     modelMatch: (filename, member) => {
-//         // filename = filnamn :^)
-//         // member = klassnamn
-//         var val = filename.substr(0, filename.indexOf(".model")).toLowerCase() === member.toLowerCase()
-//         return val
-//     },
-//     logging: process.env.NODE_ENV?.trim() != "test"
-// })
-
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    //storage: 'backend-db.sqlite',
     models: [__dirname + "/models/*.model.ts"],
     modelMatch: (filename, member) => {
         // filename = filnamn :^)
